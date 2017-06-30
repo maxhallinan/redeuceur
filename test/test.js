b2e0fd09451418ab31cf9a35e07065584729e7e3
Max Hallinan <maxhallinan@gmail.com>
2017-06-30 19:06:43 -0400

    Write type signature tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,72 @@
 const assert = require('chai').assert;
-const redeucer = require('../dist');
+const redeuceur = require('../dist');
 
 describe('unit > createReducer', function () {
   describe('type signature', function () {
     it('Throws an Error if called without two arguments.', function () {
+      const errMsg = 
+        'Must be called with two arguments: ' +
+        'initialState and cases.';
+
+      assert.throws(() => { redeuceur(); }, Error, errMsg);
+      assert.throws(() => { redeuceur(true); }, Error, errMsg);
     });
-    it('Throws a TypeError if cases is not an array.', function () {});
-    it('Throws a TypeError if a case is not an array.', function () {});
-    it('Throws an Error if a case does not have a test and a handler.', function () {});
-    it('Throws a TypeError if a test is not a string, an array of strings, or a function.', function () {});
-    it('Returns a function.', function () {});
+
+    it('Throws a TypeError if cases is not an array.', function () {
+      const errMsg = 'Cases must be an array.';
+      const invalid = [true, 1, 'a', {}, () => {}, null];
+
+      invalid.forEach(x => {
+        assert.throws(() => { redeuceur(true, x); }, TypeError, errMsg);
+      });
+    });
+
+    it('Throws a TypeError if a case is not an array.', function () {
+      const errMsg = 'Invalid case in cases. Each case must be an array.';
+      const invalid = [true, 1, 'a', {}, () => {}, null];
+
+      invalid.forEach(x => {
+        // one invalid case
+        assert.throws(() => { 
+          redeuceur(true, [ x, ]); 
+        }, TypeError, errMsg);
+
+        // mix of invalid and valid cases
+        assert.throws(() => { 
+          const cases = [
+            ['foo', true],
+            x,
+            ['bar', false],
+          ];
+
+          redeuceur(true, cases);
+        }, TypeError, errMsg);
+      });
+    });
+
+    it('Throws an Error if a case does not have a test and a handler.', function () {
+      const errMsg = 'Each case must have a test and a handler.';
+
+      assert.throws(() => { redeuceur(true, [[]]); }, Error, errMsg);
+      assert.throws(() => { redeuceur(true, [['a']]); }, Error, errMsg);
+
+    });
+
+    it('Throws a TypeError if a case is not a valid type.', function () {
+      const errMsg = 'Invalid case. Expected a string, an array of strings, or a function.';
+      const handler = () => ({});
+      const invalid = [true, 1, {}, () => {}, null];
+
+      invalid.forEach(x => {
+        assert.throws(() => { 
+          redeuceur(true, [x, handler]);
+        }, Error, errMsg);
+      });
+    });
+
+    it('Returns a function.', function () {
+      assert.isFunction(redeuceur(true, ['x', true]));
+    });
   });
 
   describe('reducer function', function () {
